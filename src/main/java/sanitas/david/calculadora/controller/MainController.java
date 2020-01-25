dd2600ce8155a35efcdcf88e6d04b412e48d6bf8
david.corrales <david.corrales@innopulse.es>
2020-01-25 17:44:54 +0100

    Se añade log4j2 por comodidad. Se comienza a implementar los parámetros.

---
+++ src/main/java/sanitas/david/calculadora/controller/MainController.java
@@ -0,0 +1,43 @@
+package sanitas.david.calculadora.controller;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	final static Logger logger = LogManager.getLogger(MainController.class);
+
+	@GetMapping("calculadora")
+	int calculadoraEntrada(@RequestParam(value = "operador1") String op1, @RequestParam(value = "operador2") String op2,
+			@RequestParam(value = "operacion") String operacion) {
+		
+		int resultado = 0;
+		boolean opOk1 = comprobarParametros(op1);
+		boolean opOk2 = comprobarParametros(op2);
+		
+		if(opOk1 && opOk2) {
+			
+		} else {
+			
+			if(!opOk1) logger.error("Parametro 1 no numérico");
+			if(!opOk2) logger.error("Parametro 2 no numérico");
+		}
+		
+		return resultado;
+	}
+	
+	private boolean comprobarParametros(String param) {
+		
+		try {
+			Integer.parseInt(param);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		
+	}
+}
